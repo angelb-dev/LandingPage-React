d5d202ec97a343c3b78813ead6110d0074a37849
Angel <angelbeneman801@gmail.com>
2024-08-15 20:44:02 -0300

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import HomeContainer from './containers/HomeContainer';
+import HomeContainer from './containers/HomeContainer/HomeContainer';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
