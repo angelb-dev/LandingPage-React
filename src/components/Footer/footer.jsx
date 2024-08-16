c8f60a9911393d8539c2c25b13000d0f8c83cdb0
Angel <angelbeneman801@gmail.com>
2024-08-16 15:23:32 -0300

    updated

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__text">&copy; 2024 My React Project</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/containers/HomeContainer/HomeContainer.jsx
+++ src/containers/HomeContainer/HomeContainer.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer';
 import './HomeContainer.css';
 
 const HomeContainer = () => {
   return (
-    <div className="home-container">
-      <Header />
+    <div className="grid-container">
+      <header className="header">
+        <Header />
+      </header>
+      <main className="main-content">
+        <h1>Contenido Principal</h1>
+        <p>testeando</p>
+      </main>
+      <footer className="footer">
+        <Footer />
+      </footer>
     </div>
   );
 };
